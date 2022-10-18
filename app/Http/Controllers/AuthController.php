294339be40c5588bd7d32c27d2b68d9037390285
NAUFALYANUARDHI26 <92080587+NAUFALYANUARDHI26@users.noreply.github.com>
2022-10-18 10:52:09 +0700

    belajar endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    
+public function me(){
+    return[
+        'NIS' => 3103120160,
+        'Nama' => 'naufal yanuardhi',
+        'phone' => '0859148460441',
+        'Kelas' => 'XII RPL 5',
+        
+    ];
+}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,25 @@ Route::get('/', function () {
 Route::get('cobaa', function () {
     return ('haii');
 });
+
+Route::get('coba1', function(){
+    return ['petra', 'starry', 'mus'];
+});
+
+Route::get('coba2', function(){
+    return[
+        'Nama' => 'naufal yanuardhi',
+        'Kelas' => 'XII RPL 5',
+        'NIS' => 3103120160
+    ];
+});
+
+Route::get('coba3', function(){
+    return response()->json(
+        [
+            'Nama' => 'Naufal yanuardhi',
+            'Kelas' => 'XII RPL 5',
+            'NIS' => 3103120160
+        ],201
+    );
+});
