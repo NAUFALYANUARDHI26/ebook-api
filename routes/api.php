05f938de5e9658d030c66a31c4c02b3587da1a59
NAUFALYANUARDHI26 <92080587+NAUFALYANUARDHI26@users.noreply.github.com>
2022-11-01 13:27:37 +0700

    Authentication

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,8 +24,26 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('me', [AuthController::class, 'me']);
 
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'show']);
-Route::post('/books', [BookController::class, 'store']);
-Route::put('/books/{id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class, 'detroy']);
+//Route::get('/books', [BookController::class, 'index']);
+//Route::get('/books/{id}', [BookController::class, 'show']);   
+//Route::post('/books', [BookController::class, 'store']);
+//Route::put('/books/{id}', [BookController::class, 'update']);
+//Route::delete('/books/{id}', [BookController::class, 'detroy']);
+
+
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/authors/{id}', [AuthorController::class, 'show']);
+Route::post('/authors', [AuthorController::class, 'store']);
+Route::put('/authors/{id}', [AuthorController::class, 'update']);
+Route::delete('/authors/{id}', [AuthorController::class, 'detroy']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function (){
+
+    Route::resource('books', BookController::class)->except( ['create', 'edit', 'index', 'show']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except( ['create', 'edit', 'index', 'show']);
+});
